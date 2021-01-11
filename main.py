7de44a281b34eab9adbe975668d45cfef552097f
Aks Chunara <ak278582@dal.ca>
2021-01-10 22:09:09 -0400

    Added boolean failed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyautogui
 import win32api
 import cv2
 
+dimensions = [780, 360, 344, 580]
 X = 1130
 xPos = []
 yPos = []
@@ -15,8 +16,10 @@ timeTaken = 0
 
 keyboard.wait("h")
 while not keyboard.is_pressed('q'):
+    failed = False
     try:
-        x, y = pyautogui.locateCenterOnScreen('ball.png', region=(780, 360, 344, 580),
+        x, y = pyautogui.locateCenterOnScreen('ball.png',
+                                              region=(dimensions[0], dimensions[1], dimensions[2], dimensions[3]),
                                               grayscale=True, confidence=0.8)
         endTime = time.time()
         timeTaken = endTime - startTime
@@ -28,12 +31,13 @@ while not keyboard.is_pressed('q'):
 
         startTime = time.time()
     except:
-        1
+        failed = True
 
-    if len(xPos) == 2:
-        xSpeed = (xPos[1] - xPos[0]) / timeTaken
-        ySpeed = (yPos[1] - yPos[0]) / timeTaken
-        if xSpeed > 0:
-            xBallBoardDistance = X - x
-            y = round(((xBallBoardDistance / xSpeed) * ySpeed) + yPos[1])
-            win32api.SetCursorPos((X, y))
+    if not failed:
+        if len(xPos) == 2:
+            xSpeed = (xPos[1] - xPos[0]) / timeTaken
+            ySpeed = (yPos[1] - yPos[0]) / timeTaken
+            if xSpeed > 0:
+                xBallBoardDistance = X - x
+                y = round(((xBallBoardDistance / xSpeed) * ySpeed) + yPos[1])
+                win32api.SetCursorPos((X, y))
